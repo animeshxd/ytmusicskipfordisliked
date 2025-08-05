4b3ccc1ee60bff0c754fd9e24e73f39fb3f7a638
Animesh Murmu <am2646374@gmail.com>
2025-08-05 10:30:14 +0530

    Add build script, Update script

--- skip.js
+++ skip.js
@@ -1,17 +1,71 @@
-function handleDislike() {
-    // console.log("Content Changed");
-    const nextButton = document.querySelector('tp-yt-paper-icon-button.next-button');
-    const isDisliked = document.querySelector('ytmusic-like-button-renderer[like-status="DISLIKE"]');
 
-    if (nextButton != null && isDisliked != null) {
-        nextButton.click();
+const excludeTitle = [
+    /Jhankar/gmi
+]
+
+const debounceTime = 1000;
+
+function debounce(func, wait) {
+    let timeout;
+
+    return (...args) => {
+        try {
+            const later = () => {
+                clearTimeout(timeout);
+                func(...args);
+            };
+
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        } catch (error) {
+
+        }
+    };
+}
+
+function getTitle() {
+    return document.querySelector(".title.ytmusic-player-bar").title
+}
+
+function overrideVideoDisplayWithImage() {
+    try {
+        //TODO: make it more specific
+        const image = document.querySelector("#song-image.style-scope.ytmusic-player")
+        const video = document.querySelector("#song-video.style-scope.ytmusic-player")
+        image.style.removeProperty("display");
+        video.style.display = "none";
+    } catch (error) {
+
     }
 }
 
-const observer = new MutationObserver(handleDislike);
+function onChange() {
+    try {
+        overrideVideoDisplayWithImage()
+        const nextButton = document.querySelector('yt-icon-button.next-button.style-scope.ytmusic-player-bar');
+        const title = getTitle();
+        const dislikeButton = document.querySelector("ytmusic-like-button-renderer.ytmusic-player-bar > #button-shape-dislike > button")
 
-observer.observe(document.body, { childList: true, subtree: false });
+        if (!nextButton && !title) return;
+        const containsExcluded = title && excludeTitle.some((r) => !!title.match(r))
+        const isDisliked = !!document.querySelector('ytmusic-like-button-renderer[like-status="DISLIKE"]');
+        // console.debug({ containsExcluded, isDisliked, dislikeButton })
+        if (containsExcluded && !isDisliked && !!dislikeButton && getTitle() == title) {
+            dislikeButton.click();
+            // console.debug("dislikeButton.click()")
+            return;
+        }
+        if (nextButton != null && isDisliked && getTitle() == title) {
+            // console.debug("nextButton.click()")
+            nextButton.click();
+        }
+    } catch (err) {
+        // console.error(err)
+    }
+}
 
-handleDislike();
+var observer = new MutationObserver(debounce(onChange, debounceTime));
 
+observer.observe(document.body, { childList: true, subtree: true });
 
+onChange();
